15aec94ed268a84b66f5806984b2586afc16bb33
Nishant-Halwai <nishanthalwai52@gmail.com>
2024-02-14 10:53:34 +0530

    first commit

---
+++ AlphaHttpTrigger1.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.AlphaFunction
+{
+    public class AlphaHttpTrigger1
+    {
+        private readonly ILogger<AlphaHttpTrigger1> _logger;
+
+        public AlphaHttpTrigger1(ILogger<AlphaHttpTrigger1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("AlphaHttpTrigger1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
